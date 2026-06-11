5d56ab5372379e05d8d60a273f6df42049fc31be
John Harvieux <jharvieux@gmail.com>
2026-06-11 06:03:14 -0500

    fix: add canonical_match_reviews authenticated grant, fix personal_access_tokens order, add vendor_health to grants snapshot

--- db/grants-snapshot-main.sql
+++ db/grants-snapshot-main.sql
@@ -71,6 +71,7 @@ GRANT DELETE, INSERT, SELECT, UPDATE ON public.campaigns TO authenticated;
 GRANT DELETE, INSERT, SELECT, UPDATE ON public.campaigns TO service_role;
 
 -- TABLE: public.canonical_match_reviews
+GRANT SELECT, UPDATE ON public.canonical_match_reviews TO authenticated;
 GRANT DELETE, INSERT, SELECT, UPDATE ON public.canonical_match_reviews TO service_role;
 
 -- TABLE: public.ccpa_deletion_executions
@@ -266,7 +267,7 @@ GRANT SELECT ON public.persona_safety_config TO authenticated;
 GRANT SELECT, UPDATE ON public.persona_safety_config TO service_role;
 
 -- TABLE: public.personal_access_tokens
-GRANT SELECT, INSERT, UPDATE ON public.personal_access_tokens TO service_role;
+GRANT INSERT, SELECT, UPDATE ON public.personal_access_tokens TO service_role;
 
 -- TABLE: public.personas
 GRANT SELECT ON public.personas TO authenticated;
@@ -459,6 +460,9 @@ GRANT INSERT, SELECT, UPDATE ON public.user_data_export_requests TO service_role
 -- TABLE: public.users
 GRANT DELETE, INSERT, SELECT, UPDATE ON public.users TO authenticated;
 GRANT DELETE, INSERT, SELECT, UPDATE ON public.users TO service_role;
+
+-- TABLE: public.vendor_health
+GRANT INSERT, SELECT, UPDATE ON public.vendor_health TO service_role;
 
 -- TABLE: public.voice_profiles
 GRANT INSERT, SELECT, UPDATE ON public.voice_profiles TO authenticated;
